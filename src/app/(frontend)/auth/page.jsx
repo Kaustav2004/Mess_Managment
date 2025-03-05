4b3a98330faaa1e023165a0ff18d7d0c2db1ffc8
Diptesh Karmakar <dipteshkarmakar007@gmail.com>
2025-03-05 14:40:55 +0530

    auth page designed

---
+++ src/app/(frontend)/auth/page.jsx
@@ -0,0 +1,36 @@
+"use client"
+import { useState } from "react"
+import SignupCard from "./signup"
+import SigninCard from "./signin"
+import Image from "next/image"
+
+export default function Auth() {
+  const [reg, setReg] = useState(false)
+
+  return (
+    <div className="flex">
+      {/* Left Side - Image (Hidden on small screens) */}
+      <div className="div1 hidden xl:block w-1/2 h-screen bg-amber-200">
+        <Image
+          src="/auth_page_image.jpeg"
+          width={500}
+          height={500}
+          alt="food"
+          className="object-cover w-full h-full"
+        />
+      </div>
+
+      {/* Right Side - Auth Forms (Takes full width when the left div is hidden) */}
+      <div className="bg-gradient-to-l from-gray-900 via-gray-800 to-black  flex flex-col justify-center items-center w-full xl:w-1/2 h-screen">
+        {reg ? <SigninCard /> : <SignupCard />}
+
+        <span
+          onClick={() => setReg(!reg)}
+          className="text-blue-500 cursor-pointer mt-4"
+        >
+          {reg ? "New Registration" : "Already have an account? Sign In"}
+        </span>
+      </div>
+    </div>
+  )
+}
---
+++ src/app/(frontend)/auth/signup.jsx
@@ -0,0 +1,129 @@
+"use client"
+
+import { signIn, useSession } from "next-auth/react"
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
+import Card from "@mui/material/Card"
+import CardContent from "@mui/material/CardContent"
+import Button from "@mui/material/Button"
+import * as React from "react"
+import Box from "@mui/material/Box"
+import InputLabel from "@mui/material/InputLabel"
+import MenuItem from "@mui/material/MenuItem"
+import Image from "next/image"
+import FormControl from "@mui/material/FormControl"
+import Select, { SelectChangeEvent } from "@mui/material/Select"
+
+export default function SignupCard() {
+  const { data: session } = useSession()
+  const router = useRouter()
+  const [value, setValue] = useState("1")
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue)
+  }
+
+  useEffect(() => {
+    if (session) {
+      router.push("/")
+    }
+  }, [session])
+
+  const authHandler = async () => {
+    await signIn("google")
+  }
+
+  const [type, setType] = React.useState("")
+
+  return (
+    <div className="flex justify-center items-center bg-gray-100 ">
+      <Card className="w-full max-w-md p-6 shadow-lg rounded-2xl bg-white">
+        <h1 className="text-2xl font-semibold text-center text-gray-800 mb-6">
+          Welcome to Hostel Management
+        </h1>
+        <CardContent>
+          <div className="space-y-4">
+            <div className="flex gap-4">
+              <div className="flex flex-col w-1/2">
+                <label className="text-sm font-medium text-gray-600">
+                  First Name
+                </label>
+                <input
+                  type="text"
+                  className="border rounded-lg p-2 focus:ring-2 focus:ring-sky-500"
+                />
+              </div>
+              <div className="flex flex-col w-1/2">
+                <label className="text-sm font-medium text-gray-600">
+                  Last Name
+                </label>
+                <input
+                  type="text"
+                  className="border rounded-lg p-2 focus:ring-2 focus:ring-sky-500"
+                />
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <label className="text-sm font-medium text-gray-600">Email</label>
+              <input
+                type="email"
+                className="border rounded-lg p-2 focus:ring-2 focus:ring-sky-500"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label className="text-sm font-medium text-gray-600">
+                Password
+              </label>
+              <input
+                type="password"
+                className="border rounded-lg p-2 focus:ring-2 focus:ring-sky-500"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label className="text-sm font-medium text-gray-600">
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                className="border rounded-lg p-2 focus:ring-2 focus:ring-sky-500"
+              />
+            </div>
+
+            <Box sx={{ minWidth: 120 }}>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">
+                  Type of account
+                </InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={type}
+                  label="Type of account"
+                  onChange={handleChange}
+                >
+                  <MenuItem value={10}>Student</MenuItem>
+                  <MenuItem value={20}>Mess Committee</MenuItem>
+                  <MenuItem value={30}>Super</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+            <Button className="w-full bg-sky-500 hover:bg-sky-600 text-white font-medium py-2 rounded-lg mt-4">
+              Sign Up
+            </Button>
+          </div>
+        </CardContent>
+
+        <div
+          onClick={authHandler}
+          className="mt-2 items-center flex flex-col gap-2 cursor-pointer"
+        >
+          <h1 className="">OR</h1>
+          <div className="flex items-center gap-2 justify-center p-2 border border-gray-300 rounded-md cursor-pointer w-60">
+            <Image src="/google_logo.png" alt="google" width={30} height={30} />
+            <span>Sign up with Google</span>
+          </div>
+        </div>
+      </Card>
+    </div>
+  )
+}
